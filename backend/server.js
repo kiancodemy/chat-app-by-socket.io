23ccfe4aa01e8abf0c35e7517cbc538c5655ec96
kian kheirani <kiankheirani@gmail.com>
2024-03-31 23:19:07 +0430

    'start'

---
+++ backend/server.js
@@ -0,0 +1,22 @@
+import express from "express";
+import dotenv from "dotenv";
+dotenv.config();
+import { chats } from "./data/data.js";
+const app = express();
+app.get("/api/chat/:id", async (req, res) => {
+  try {
+    const id = req.params.id;
+    const find = chats.find((a) => a._id === id);
+    if (find) {
+      res.send(find);
+    } else {
+      throw new Error("nothing found");
+    }
+  } catch (err) {
+    res.send(err.message);
+  }
+});
+
+app.listen(process.env.PORT, () => {
+  console.log("server started");
+});

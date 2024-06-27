69518868a3f31b6471dd4eb780a046b60b47f3a6
kian kheirani <kiankheirani@gmail.com>
2024-06-27 08:45:21 +0330

    debug-5'

--- frontend/src/screen/Login.jsx
+++ frontend/src/screen/Login.jsx
@@ -95,7 +95,7 @@ function Login() {
           required: { value: true, message: "Email Is Required" },
         })}
         sx={{
-          "&:input:-internal-autofill-selected": {
+          "&:-internal-autofill-selected": {
             backgroundColor: "white",
           },
           "& .MuiInputLabel-root.Mui-focused": {
@@ -127,7 +127,7 @@ function Login() {
           required: { value: true, message: "Password Is Required" },
         })}
         sx={{
-          "&:input:-internal-autofill-selected": {
+          "&:-internal-autofill-selected": {
             backgroundColor: "white",
           },
           "& .MuiInputLabel-root.Mui-focused": {
